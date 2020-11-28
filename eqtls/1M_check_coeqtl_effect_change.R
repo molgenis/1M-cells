5552da6ecd5ecc75835346683fe0aba04b05c425
Roy Oelen <r.oelen@umcg.nl>
2020-11-28 17:52:58 +0100

    - plot the concordances of the effect sizes - check if stim r is statistically different

--- eqtls/1M_check_coeqtl_effect_change.R
+++ eqtls/1M_check_coeqtl_effect_change.R
@@ -201,6 +201,187 @@ get_coeqtl_plots_matched <- function(r_v2_stim, r_v3_stim, r_v2_ut, r_v3_ut, p_m
   return(stim_plots)
 }
 
+plot_concordances <- function(r_v2, r_v3, p_meta=NULL, p_v2=NULL, p_v3=NULL, filter_by_ribosomal=F, timepoints=c('3h', '24h')){
+  # create list of plots
+  stim_plots <- list()
+  # check against each condition
+  for(timepoint in timepoints){
+    # grab the conditions
+    conditions <- colnames(r_v2)[grep(timepoint, colnames(r_v2))]
+    # go through the conditions
+    for(i in 1:length(conditions)){
+      # against that other condition
+      for(i2 in i:length(conditions)){
+        # get the conditions
+        condition1 <- conditions[i]
+        condition2 <- conditions[i2]
+        # initialise values
+        r_v2_sig_condition <- NULL
+        r_v3_sig_condition <- NULL
+        # check by meta p as the first priority
+        if(!is.null(p_meta)){
+          print(paste(condition1, 'sig:', p_meta['significance_threshold', condition1]))
+          print(paste(condition2, 'sig:', p_meta['significance_threshold', condition2]))
+          # confine to those genes that are more significant than the threshold states in both conditions
+          meta_sig_condition <- p_meta[!is.na(p_meta[[condition1]]) & 
+                                         p_meta[[condition1]] < p_meta['significance_threshold', condition1] & 
+                                         !is.na(p_meta[[condition2]]) & 
+                                         p_meta[[condition2]] < p_meta['significance_threshold', condition2], ]
+          # filter by ribosomal if requested
+          if(nrow(meta_sig_condition) > 0 & filter_by_ribosomal){
+            meta_sig_condition <- meta_sig_condition[startsWith(rownames(meta_sig_condition), 'RPS') | startsWith(rownames(meta_sig_condition), 'RPL'), ]
+          }
+          # only if there are genes left, can we continue
+          if(nrow(meta_sig_condition) > 0){
+            # grab the r values of those genes from the other dataframe
+            r_v2_sig_condition <- r_v2[rownames(meta_sig_condition), ]
+            # order by the UT R for plotting purposes later
+            r_v2_sig_condition <- r_v2_sig_condition[order(r_v2_sig_condition[[condition1]]), ]
+            # same for v3 sampels
+            r_v3_sig_condition <- r_v3[rownames(meta_sig_condition), ]
+            r_v3_sig_condition <- r_v3_sig_condition[order(r_v3_sig_condition[[condition1]]), ]
+          }
+        }
+        # if there is no meta p dataframe, check if there is a v2 and v3 dataframe
+        else if(!is.null(p_v2) & !is.null(p_v3)){
+          print(paste(condition1, 'sig:', p_v2['significance_threshold', condition1]))
+          print(paste(condition2, 'sig:', p_v2['significance_threshold', condition2]))
+          # subset to genes significant below the significance threshold in both conditions
+          v2_sig_condition <- p_v2[!is.na(p_v2[[condition1]]) & 
+                                         p_v2[[condition1]] < p_v2['significance_threshold', condition1] & 
+                                         !is.na(p_v2[[condition2]]) & 
+                                         p_v2[[condition2]] < p_v2['significance_threshold', condition2], ]
+          # filter by ribosomal if requested
+          if(filter_by_ribosomal & nrow(v2_sig_condition) > 0){
+            v2_sig_condition <- v2_sig_condition[startsWith(rownames(v2_sig_condition), 'RPS') | startsWith(rownames(v2_sig_condition), 'RPL'), ]
+          }
+          # subset to significant genes if possible
+          if(nrow(v2_sig_condition) > 0){
+            r_v2_sig_condition <- r_v2[rownames(v2_sig_condition), ]
+          }
+          print(paste(condition1, 'sig:', p_v3['significance_threshold', condition1]))
+          print(paste(condition2, 'sig:', p_v3['significance_threshold', condition2]))
+          # subset to genes significant below the significance threshold in both conditions
+          v3_sig_condition <- p_v3[!is.na(p_v3[[condition1]]) & 
+                                     p_v3[[condition1]] < p_v3['significance_threshold', condition1] & 
+                                     !is.na(p_v3[[condition2]]) & 
+                                     p_v3[[condition2]] < p_v3['significance_threshold', condition2], ]
+          # filter by ribosomal if requested
+          if(filter_by_ribosomal & nrow(v3_sig_condition) > 0){
+            v3_sig_condition <- v3_sig_condition[startsWith(rownames(v3_sig_condition), 'RPS') | startsWith(rownames(v3_sig_condition), 'RPL'), ]
+          }
+          if(nrow(v3_sig_condition) > 0){
+            r_v3_sig_condition <- r_v3[rownames(v3_sig_condition), ]
+          }
+        }
+        else{
+          print('use either meta p or v2 and v3 p values')
+        }
+        # set colors
+        colors_list <- as.list(c('grey', 'red', 'green'))
+        names(colors_list) <- c('-', 'stronger', 'weaker')
+        # if we were able to create a dataframe with significant v2 r values for the UT and specific stim
+        if(!is.null(r_v2_sig_condition)){
+          # turn into a plot dataframe
+          plot_df_v2 <- data.frame(condition1_rscore = r_v2_sig_condition[[condition1]], condition2_rscore = r_v2_sig_condition[[condition2]])
+          # annotate to state if the effect after stim is weaker or stronger
+          plot_df_v2$dir <- '-'
+          if(nrow(plot_df_v2[(plot_df_v2$condition1_rscore > 0 & plot_df_v2$condition2_rscore > plot_df_v2$condition1_rscore) | (plot_df_v2$condition1_rscore < 0 & plot_df_v2$condition2_rscore < plot_df_v2$condition1_rscore), ]) > 0){
+            plot_df_v2[(plot_df_v2$condition1_rscore > 0 & plot_df_v2$condition2_rscore > plot_df_v2$condition1_rscore) | (plot_df_v2$condition1_rscore < 0 & plot_df_v2$condition2_rscore < plot_df_v2$condition1_rscore), ]$dir <- 'stronger'
+          }
+          if(nrow(plot_df_v2[(plot_df_v2$condition1_rscore > 0 & plot_df_v2$condition2_rscore < plot_df_v2$condition1_rscore) | (plot_df_v2$condition1_rscore < 0 & plot_df_v2$condition2_rscore > plot_df_v2$condition1_rscore), ]) > 0){
+            plot_df_v2[(plot_df_v2$condition1_rscore > 0 & plot_df_v2$condition2_rscore < plot_df_v2$condition1_rscore) | (plot_df_v2$condition1_rscore < 0 & plot_df_v2$condition2_rscore > plot_df_v2$condition1_rscore), ]$dir <- 'weaker'
+          }
+          # create the plot
+          stim_plot_v2 <- ggplot(plot_df_v2, aes(x=condition1_rscore, y=condition2_rscore, color=dir)) +
+            geom_point() +
+            ggtitle(paste(condition1, 'vs', condition2, 'R')) +
+            labs(y = condition2, x=condition1) +
+            scale_color_manual(values=colors_list)
+          # save the plot output
+          stim_plots[[paste('v2', condition1, condition2, sep = '_')]] <- stim_plot_v2
+        }
+        # if we were able to create a dataframe with significant v3 r values for the UT and specific stim
+        if(!is.null(r_v3_sig_condition)){
+          # turn into a plot dataframe
+          plot_df_v3 <- data.frame(condition1_rscore = r_v3_sig_condition[[condition1]], condition2_rscore = r_v3_sig_condition[[condition2]])
+          # annotate to state if the effect after stim is weaker or stronger
+          plot_df_v3$dir <- '-'
+          if(nrow(plot_df_v3[(plot_df_v3$condition1_rscore > 0 & plot_df_v3$condition2_rscore > plot_df_v3$condition1_rscore) | (plot_df_v3$condition1_rscore < 0 & plot_df_v3$condition2_rscore < plot_df_v3$condition1_rscore), ]) > 0){
+            plot_df_v3[(plot_df_v3$condition1_rscore > 0 & plot_df_v3$condition2_rscore > plot_df_v3$condition1_rscore) | (plot_df_v3$condition1_rscore < 0 & plot_df_v3$condition2_rscore < plot_df_v3$condition1_rscore), ]$dir <- 'stronger'
+          }
+          if(nrow(plot_df_v3[(plot_df_v3$condition1_rscore > 0 & plot_df_v3$condition2_rscore < plot_df_v3$condition1_rscore) | (plot_df_v3$condition1_rscore < 0 & plot_df_v3$condition2_rscore > plot_df_v3$condition1_rscore), ]) > 0){
+            plot_df_v3[(plot_df_v3$condition1_rscore > 0 & plot_df_v3$condition2_rscore < plot_df_v3$condition1_rscore) | (plot_df_v3$condition1_rscore < 0 & plot_df_v3$condition2_rscore > plot_df_v3$condition1_rscore), ]$dir <- 'weaker'
+          }
+          # create the plot
+          stim_plot_v3 <- ggplot(plot_df_v3, aes(x=condition1_rscore, y=condition2_rscore, color=dir)) +
+            geom_point() +
+            ggtitle(paste(condition1, 'vs', condition2, 'R')) +
+            labs(y = condition2, x=condition1) +
+            scale_color_manual(values=colors_list)
+          # save the plot output
+          stim_plots[[paste('v3', condition1, condition2, sep = '_')]] <- stim_plot_v3
+        }
+      }
+    }
+  }
+  return(stim_plots)
+}
+
+get_coeqtl_differences <- function(r_v2_stim, r_v3_stim, r_v2_ut, r_v3_ut, p_meta_ut=NULL, p_meta_stim=NULL, p_v2_ut=NULL, p_v2_stim=NULL, p_v3_ut=NULL, p_v3_stim=NULL, filter_by_ribosomal=F, stim_conditions = c('X3hCA', 'X24hCA', 'X3hMTB', 'X24hMTB', 'X3hPA', 'X24hPA')){
+  # create list of plots
+  stim_results <- list()
+  # check against each condition
+  for(stim_condition in stim_conditions){
+    # initialise values
+    r_v2_sig_condition <- NULL
+    r_v3_sig_condition <- NULL
+    # use meta
+    if(!is.null(p_meta_ut) & !is.null(p_meta_stim)){
+      # print the significances
+      print(paste('UT sig:', p_meta_ut['significance_threshold', paste('UT', stim_condition, sep = '_')]))
+      print(paste(stim_condition, 'sig:', p_meta_stim['significance_threshold', stim_condition]))
+      # get the UT significant genes
+      ut_sig_genes <- rownames(p_meta_ut[!is.na(p_meta_ut[[paste('UT', stim_condition, sep = '_')]]) &
+                                           p_meta_ut[[paste('UT', stim_condition, sep = '_')]] < p_meta_ut['significance_threshold', paste('UT', stim_condition, sep = '_')], ])
+      stim_sig_genes <- rownames(p_meta_stim[!is.na(p_meta_stim[[stim_condition]]) &
+                                               p_meta_stim[[stim_condition]] < p_meta_stim['significance_threshold', stim_condition], ])
+      common_sig_genes <- intersect(ut_sig_genes, stim_sig_genes)
+      # filter by ribosomal if requested
+      if(length(common_sig_genes) > 0 & filter_by_ribosomal){
+        common_sig_genes <- common_sig_genes[which(startsWith(common_sig_genes, 'RPS') | startsWith(common_sig_genes, 'RPL'))]
+      }
+      if(length(common_sig_genes) > 0){
+        # get the stim r sigs
+        r_v2_sig_condition <- r_v2_stim[common_sig_genes, stim_condition, drop = F]
+        r_v3_sig_condition <- r_v3_stim[common_sig_genes, stim_condition, drop = F]
+        # need to get the ut r sigs
+        r_v2_sig_condition <- cbind(r_v2_sig_condition, r_v2_ut[common_sig_genes, paste('UT', stim_condition, sep = '_'), drop = F])
+        r_v3_sig_condition <- cbind(r_v3_sig_condition, r_v3_ut[common_sig_genes, paste('UT', stim_condition, sep = '_'), drop = F])
+        # set the colnames for the plots
+        colnames(r_v2_sig_condition) <- c(stim_condition, 'UT')
+        colnames(r_v3_sig_condition) <- c(stim_condition, 'UT')
+      }
+    }
+    # set colors
+    colors_list <- as.list(c('grey', 'red', 'green'))
+    names(colors_list) <- c('-', 'stronger', 'weaker')
+    # if we were able to create a dataframe with significant v2 r values for the UT and specific stim
+    if(!is.null(r_v2_sig_condition)){
+      # test if different
+      v2_res <- wilcox.test(r_v2_sig_condition$UT, r_v2_sig_condition[[stim_condition]], alternative = 'g')
+      stim_results[[paste('v2', stim_condition, sep = '_')]] <- v2_res
+    }
+    # if we were able to create a dataframe with significant v2 r values for the UT and specific stim
+    if(!is.null(r_v3_sig_condition)){
+      # test if different
+      v3_res <- wilcox.test(r_v3_sig_condition$UT, r_v3_sig_condition[[stim_condition]], alternative = 'g')
+      stim_results[[paste('v3', stim_condition, sep = '_')]] <- v3_res
+    }
+  }
+  return(stim_results)
+}
+
 
 # locations of the files
 p_meta_loc <- '/data/scRNA/eQTL_mapping/coexpressionQTLs/output_RPS26_confine_meta_20201124/RPS26_mono_meta_p.tsv'
@@ -252,6 +433,14 @@ stim_plots_ribosomal_per_chem <- get_coeqtl_plots(r_v2, r_v3, p_meta=NULL, p_v2=
 stim_matched_plots <- get_coeqtl_plots_matched(r_v2_stim=r_v2_matched_stim, r_v3_stim=r_v3_matched_stim, r_v2_ut=r_v2_matched_ut, r_v3_ut=r_v3_matched_ut, p_meta_ut=p_meta_matched_ut, p_meta_stim=p_meta_matched_stim, p_v2_ut=NULL, p_v2_stim=NULL, p_v3_ut=NULL, p_v3_stim=NULL, filter_by_ribosomal=F, stim_conditions = c('X3hCA', 'X24hCA', 'X3hMTB', 'X24hMTB', 'X3hPA', 'X24hPA'))
 stim_matched_plots_ribosomal <- get_coeqtl_plots_matched(r_v2_stim=r_v2_matched_stim, r_v3_stim=r_v3_matched_stim, r_v2_ut=r_v2_matched_ut, r_v3_ut=r_v3_matched_ut, p_meta_ut=p_meta_matched_ut, p_meta_stim=p_meta_matched_stim, p_v2_ut=NULL, p_v2_stim=NULL, p_v3_ut=NULL, p_v3_stim=NULL, filter_by_ribosomal=T, stim_conditions = c('X3hCA', 'X24hCA', 'X3hMTB', 'X24hMTB', 'X3hPA', 'X24hPA'))
 
+# check concordances
+concordances_plots <- plot_concordances(r_v2, r_v3, p_meta=p_meta, p_v2=NULL, p_v3=NULL, filter_by_ribosomal=F, timepoints=c('3h', '24h'))
+concordances_plots_ribo <- plot_concordances(r_v2, r_v3, p_meta=p_meta, p_v2=NULL, p_v3=NULL, filter_by_ribosomal=T, timepoints=c('3h', '24h'))
+concordances_plots_ribo_per_chem <- plot_concordances(r_v2=r_v2, r_v3=r_v3, p_meta=NULL, p_v2=p_v2, p_v3=p_v3, filter_by_ribosomal=T, timepoints=c('3h', '24h'))
+
+# check the statistical difference
+stim_matched_stats_ribosomal <- get_coeqtl_differences(r_v2_stim=r_v2_matched_stim, r_v3_stim=r_v3_matched_stim, r_v2_ut=r_v2_matched_ut, r_v3_ut=r_v3_matched_ut, p_meta_ut=p_meta_matched_ut, p_meta_stim=p_meta_matched_stim, p_v2_ut=NULL, p_v2_stim=NULL, p_v3_ut=NULL, p_v3_stim=NULL, filter_by_ribosomal=T, stim_conditions = c('X3hCA', 'X24hCA', 'X3hMTB', 'X24hMTB', 'X3hPA', 'X24hPA'))
+
 
 # create the arranged plot
 condition_plot_v2 <- ggarrange(stim_plots[['v2_X3hCA']],stim_plots[['v2_X3hMTB']], stim_plots[['v2_X3hPA']] , stim_plots[['v2_X24hCA']], stim_plots[['v2_X24hMTB']], stim_plots[['v2_X24hPA']],
@@ -277,4 +466,8 @@ condition_matched_plot_v2_ribo <- ggarrange(stim_matched_plots_ribosomal[['v2_X3
 
 condition_matched_plot_v3_ribo <- ggarrange(stim_matched_plots_ribosomal[['v3_X3hCA']],stim_matched_plots_ribosomal[['v3_X3hMTB']], stim_matched_plots_ribosomal[['v3_X3hPA']] , stim_matched_plots_ribosomal[['v3_X24hCA']], stim_matched_plots_ribosomal[['v3_X24hMTB']], stim_matched_plots_ribosomal[['v3_X24hPA']],
                                     ncol = 3, nrow = 2)
+
+condition_concordance_v2 <- ggarrange(concordances_plots_ribo_per_chem[['v2_X3hCA_X3hMTB']], concordances_plots_ribo_per_chem[['v2_X3hCA_X3hPA']], concordances_plots_ribo_per_chem[['v2_X3hMTB_X3hPA']], concordances_plots_ribo_per_chem[['v2_X24hCA_X24hPA']], concordances_plots_ribo_per_chem[['v2_X24hCA_X24hMTB']], concordances_plots_ribo_per_chem[['v2_X24hMTB_X24hPA']])
+
+condition_concordance_v3 <- ggarrange(concordances_plots_ribo_per_chem[['v3_X3hCA_X3hMTB']], concordances_plots_ribo_per_chem[['v3_X3hCA_X3hPA']], concordances_plots_ribo_per_chem[['v3_X3hMTB_X3hPA']], concordances_plots_ribo_per_chem[['v3_X24hCA_X24hPA']], concordances_plots_ribo_per_chem[['v3_X24hCA_X24hMTB']], concordances_plots_ribo_per_chem[['v3_X24hMTB_X24hPA']])
 
